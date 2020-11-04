d88ae668f4f0926760f3497921e7158f3683289e
lulopez <lulopez@estudiantec.cr>
2020-11-04 13:03:42 -0600

    [FIX] destReg -> destRego

--- CPU/PipelineEx.sv
+++ CPU/PipelineEx.sv
@@ -1,13 +1,13 @@
 module PipelineEx( input clk, rst,
 						input[31:0]R1i,R2i,R3i,input[3:0]destRegi,input[2:0] ALUinsi,input wmemi,rmemi,wregi,immi,
-						output logic[31:0]R1o,R2o,R3o, output logic[3:0]destReg,
+						output logic[31:0]R1o,R2o,R3o, output logic[3:0]destRego,
 						output logic[2:0] ALUinso,output logic wmemo,rmemo,wrego,immo);
 	always_ff@(posedge clk or posedge rst)
 		if(rst) begin
 		R1o = 32'h00;
 		R2o = 32'h00;
 		R3o = 32'h00;
-		destReg = 4'b0000;
+		destRego = 4'b0000;
 		ALUinso = 2'b00;
 		wmemo = 1'b0;
 		rmemo = 1'b0;
