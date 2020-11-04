07892363b0eae8889a8d91756c53caaedbf50a14
lulopez <lulopez@estudiantec.cr>
2020-11-04 11:59:10 -0600

    [ADD] Quartus files w/ Control Unit + Pipeline

---
+++ ExtendDeco.sv
@@ -0,0 +1,12 @@
+module ExtendDeco(input [3:0] in, output [1:0] ALUControl);
+	always @(in) begin
+		case(in)
+			4'b1011 : ALUControl = 2'b11; //19
+			4'b1100 : ALUControl = 2'b11; //19
+			4'b1010 : ALUControl = 2'b10; //23
+			4'b0000 : ALUControl = 2'b11; //28
+			4'b0011 : ALUControl = 2'b11; //28
+			default : ALUControl = 2'b00; //NONE
+		endcase
+	end
+endmodule
---
+++ PipelineEx.sv
@@ -0,0 +1,28 @@
+module PipelineEx( input clk, rst,
+						input[31:0]R1i,R2i,R3i,input[3:0]destRegi,input[2:0] ALUinsi,input wmemi,rmemi,wregi,immi,
+						output logic[31:0]R1o,R2o,R3o output logic[3:0]destRegi,
+						output logic[2:0] ALUinso,output logic wmemo,rmemo,wrego,immo);
+	always_ff@(posedge clk or posedge rst)
+		if(rst) begin
+		R1o = 32'h00;
+		R2o = 32'h00;
+		R3o = 32'h00;
+		destReg = 4'b0000;
+		ALUinso = 2'b00;
+		wmemo = 1'b0;
+		rmemo = 1'b0;
+		wrego = 1'b0;
+		immo = 1'b0;
+		end
+		else begin
+		R1o = R1i;
+		R2o = R2i;
+		R3o = R3i;
+		destRego = destRegi;
+		ALUinso = ALUinsi;
+		wmemo = wmemi;
+		rmemo = rmemi;
+		wrego = wregi;
+		immo = immi;
+		end
+endmodule
