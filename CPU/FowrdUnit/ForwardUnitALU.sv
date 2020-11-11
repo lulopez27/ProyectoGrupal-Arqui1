0cd5b6eb8ebd6fd5ff2fae3315587466c949d253
Kevin <skryfall@gmail.com>
2020-11-10 21:08:00 -0600

    Fixes

--- CPU/FowrdUnit/ForwardUnitALU.sv
+++ CPU/FowrdUnit/ForwardUnitALU.sv
@@ -7,6 +7,6 @@ module ForwardUnitALU(input [31:0]R2res3,R3res3,input [3:0]R2_2,R3_2,input[1:0]E
 	Mux2 #(32)ExR2(Wbres2,Res,(R2_2 == DestR_3)&~ExtndSel1[1],R2res4);
 	
 	logic [31:0]Wbres3;
-	Mux2 #(32)WbR3(R3res3,Res1,(R3_2  == DestR_4 )&~ExtndSel1[1]&~ExtndSel1[1],Wbres3);
-	Mux2 #(32)ExR3(Wbres3,Res,(R3_2 == DestR_3)&~ExtndSel1[1]&~ExtndSel1[1],R3res4);
+	Mux2 #(32)WbR3(R3res3,Res1,(R3_2  == DestR_4 )&~ExtndSel1[1]&~ExtndSel1[0],Wbres3);
+	Mux2 #(32)ExR3(Wbres3,Res,(R3_2 == DestR_3)&~ExtndSel1[1]&~ExtndSel1[0],R3res4);
 endmodule 
