0d25847499210e5ec5ebc79337cf9c9da8acd3f7
lulopez <lulopez@estudiantec.cr>
2020-11-10 20:49:25 -0600

    [Fix] ExtndSel

--- CPU/ControlUnit/ControlUnit.sv
+++ CPU/ControlUnit/ControlUnit.sv
@@ -3,7 +3,7 @@ module ControlUnit(input[1:0] op, input[1:0] inst, input immin,
 	logic[4:0] ALUinst;
 	assign ALUinst = {op, inst, immin};
 	logic[3:0] ExtSel;
-	assign ExtSel = {op, inst};
+	assign ExtSel = {op, inst,immin};
 	assign wmem = op[1]&~op[0]&~inst[1]&~inst[0]&~immin;
 	assign rmem = op[1]&~op[0]&~inst[1]&inst[0]&~immin;
 	assign wreg = (op[1]&op[0])|(op[1]&~op[0]&~(inst[1]&inst[0]));
--- CPU/Decoders/ExtendDeco.sv
+++ CPU/Decoders/ExtendDeco.sv
@@ -1,12 +1,16 @@
-module ExtendDeco(input [3:0] in, output logic [1:0] ALUControl);
+module ExtendDeco(input [4:0] in, output logic [1:0] ALUControl);
 	always @(in) begin
 		case(in)
-			4'b1011 : ALUControl = 2'b01; //19
-			4'b1100 : ALUControl = 2'b01; //19
-			4'b1010 : ALUControl = 2'b10; //23
-			4'b0000 : ALUControl = 2'b11; //28
-			4'b0011 : ALUControl = 2'b11; //28
-			default : ALUControl = 2'b00; //NONE
+			5'b10010 : ALUControl = 2'b01; //19
+			5'b10100 : ALUControl = 2'b01; //19
+			5'b11001 : ALUControl = 2'b01; //19
+			5'b10111 : ALUControl = 2'b01; //19
+			5'b01000 : ALUControl = 2'b01; //19
+			5'b10101 : ALUControl = 2'b10; //23
+			5'b00001 : ALUControl = 2'b11; //27
+			5'b00111 : ALUControl = 2'b11; //27
+			5'b00011 : ALUControl = 2'b11; //27
+			default : ALUControl = 2'b00; //15
 		endcase
 	end
 endmodule 
