1f152305ed23766ec767f446d48949fdc918bd23
Kevin <skryfall@gmail.com>
2020-11-12 16:53:54 -0600

    Fix

--- CPU/Decoders/ControlUnitDeco.sv
+++ CPU/Decoders/ControlUnitDeco.sv
@@ -4,6 +4,7 @@ module ControlUnitDeco(input [4:0] in, output logic [2:0] ALUControl);
 			5'b11000 : ALUControl = 3'b000; //ADD
 			5'b00001 : ALUControl = 3'b000; //JMP
 			5'b00111 : ALUControl = 3'b000; //JEQ
+			5'b00011 : ALUControl = 3'b000; //JGT
 			5'b11010 : ALUControl = 3'b001; //SUB
 			5'b10110 : ALUControl = 3'b001; //CMPR
 			5'b10111 : ALUControl = 3'b001; //CMPI
